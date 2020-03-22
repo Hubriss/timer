7beb99d840168991d3ef9069b4bd934981a9c51a
Ohan98 <35526577+Ohan98@users.noreply.github.com>
2020-03-22 20:28:11 +0100

    Add files via upload

--- Timer/index.js
+++ Timer/index.js
@@ -1,8 +1,12 @@
-
+var timeIntervalUp;
+function startTimer(){
+  timeIntervalUp = setInterval(function(){timer()}, 1000);
+}
 function timer(){
   var retrieve=document.getElementById('main');
   var number=Number(retrieve.innerHTML);
-  number++;
-  retrieve.innerHTML=number;
+  var addition = 1.00;
+  var result = number + addition;
+  var resultDec = result.toFixed(2);
+  retrieve.innerHTML=resultDec;
 }
-setInterval(timer, 1000);
